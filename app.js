089c059a853d533985b93b624d2bc260ee8e0c26
Ariq Daffa <ariq.d.harahap@banksinarmas.com>
2021-02-01 13:51:16 +0700

    added body-parser

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+// Middleware
+app.use(bodyParser.json());
 
 require("dotenv/config");
 
@@ -8,11 +12,17 @@ const api = process.env.API_URL;
 app.get(`${api}/products`, (req, res) => {
   const product = {
     id: 1,
-    name: 'hair dresser',
-    image: 'some_url'
-  }
-  
-  res.send("hello API");
+    name: "hair dresser",
+    image: "some_url",
+  };
+
+  res.send(product);
+});
+
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body;
+  console.log("newProduct", newProduct);
+  res.send(newProduct);
 });
 
 app.listen(3000, () => {
