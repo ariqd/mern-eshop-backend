a8a1805def7de97133d50e110fbfba55ef624736
Ariq Daffa <ariq.d.harahap@banksinarmas.com>
2021-02-01 15:10:30 +0700

    schema and product model

--- app.js
+++ app.js
@@ -3,36 +3,65 @@ const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const { json } = require("body-parser");
 
 // Middleware
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
 
 require("dotenv/config");
-
 const api = process.env.API_URL;
 
-app.get(`${api}/products`, (req, res) => {
-  const product = {
-    id: 1,
-    name: "hair dresser",
-    image: "some_url",
-  };
+const productSchema = mongoose.Schema({
+  name: String,
+  image: String,
+  countInStock: {
+    type: Number,
+    required: true,
+  },
+});
 
-  res.send(product);
+const Product = mongoose.model("Product", productSchema);
+
+app.get(`${api}/products`, async (req, res) => {
+  const productList = await Product.find();
+
+  if (!productList) {
+    res.status(500).json({ success: false });
+  }
+
+  res.send(productList);
 });
 
 app.post(`${api}/products`, (req, res) => {
-  const newProduct = req.body;
-  console.log("newProduct", newProduct);
-  res.send(newProduct);
+  // const newProduct = req.body;
+  // console.log("newProduct", newProduct);
+  const product = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    countInStock: req.body.countInStock,
+  });
+
+  product
+    .save()
+    .then((createdProduct) => {
+      res.status(201).json(createdProduct);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+        success: false,
+      });
+    });
+
+  // res.send(newProduct);
 });
 
 mongoose
   .connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: 'mern-eshop'
+    dbName: "mern-eshop",
   })
   .then(() => {
     console.log("Database Connection is ready...");
