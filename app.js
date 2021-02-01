3f746bf4349946de7f5689c91e7fe46427d2982b
Ariq Daffa <ariq.d.harahap@banksinarmas.com>
2021-02-01 21:16:23 +0700

    new models, routers, extend categories model

--- app.js
+++ app.js
@@ -3,59 +3,29 @@ const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const { json } = require("body-parser");
+const cors = require('cors');
+
+app.use(cors());
+app.options('*', cors());
+
+require("dotenv/config");
 
 // Middleware
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
 
-require("dotenv/config");
+// Routers
+const productsRouter = require("./routers/products");
+const categoriesRouter = require("./routers/categories");
+const ordersRouter = require("./routers/orders");
+const usersRouter = require("./routers/users");
+
 const api = process.env.API_URL;
 
-const productSchema = mongoose.Schema({
-  name: String,
-  image: String,
-  countInStock: {
-    type: Number,
-    required: true,
-  },
-});
-
-const Product = mongoose.model("Product", productSchema);
-
-app.get(`${api}/products`, async (req, res) => {
-  const productList = await Product.find();
-
-  if (!productList) {
-    res.status(500).json({ success: false });
-  }
-
-  res.send(productList);
-});
-
-app.post(`${api}/products`, (req, res) => {
-  // const newProduct = req.body;
-  // console.log("newProduct", newProduct);
-  const product = new Product({
-    name: req.body.name,
-    image: req.body.image,
-    countInStock: req.body.countInStock,
-  });
-
-  product
-    .save()
-    .then((createdProduct) => {
-      res.status(201).json(createdProduct);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-        success: false,
-      });
-    });
-
-  // res.send(newProduct);
-});
+app.use(`${api}/products`, productsRouter);
+app.use(`${api}/categories`, categoriesRouter);
+app.use(`${api}/orders`, ordersRouter);
+app.use(`${api}/users`, usersRouter);
 
 mongoose
   .connect(process.env.CONNECTION_STRING, {
@@ -72,3 +42,4 @@ app.listen(3000, () => {
   // console.log(api);
   console.log("server is running at http://localhost:3000");
 });
+
---
+++ routers/products.js
@@ -0,0 +1,39 @@
+const { Product } = require("../models/product");
+const express = require("express");
+const router = express.Router();
+
+router.get(`/`, async (req, res) => {
+  const productList = await Product.find();
+
+  if (!productList) {
+    res.status(500).json({ success: false });
+  }
+
+  res.send(productList);
+});
+
+router.post(`/`, (req, res) => {
+  // const newProduct = req.body;
+  // console.log("newProduct", newProduct);
+  const product = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    countInStock: req.body.countInStock,
+  });
+
+  product
+    .save()
+    .then((createdProduct) => {
+      res.status(201).json(createdProduct);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+        success: false,
+      });
+    });
+
+  // res.send(newProduct);
+});
+
+module.exports = router;
