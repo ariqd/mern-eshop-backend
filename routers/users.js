26249e7e0df5c4070e6fdae0ccf127fef805057a
Ariq Daffa <ariq.d.harahap@banksinarmas.com>
2021-02-02 20:46:10 +0700

    user model, add user, hash password

--- routers/users.js
+++ routers/users.js
@@ -1,5 +1,6 @@
 const { User } = require("../models/user");
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
@@ -10,6 +11,27 @@ router.get(`/`, async (req, res) => {
   }
 
   res.send(userList);
+});
+
+router.post(`/`, async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    street: req.body.street,
+    apartement: req.body.apartement,
+    city: req.body.city,
+    zip: req.body.zip,
+    country: req.body.country,
+    isAdmin: req.body.isAdmin,
+  });
+
+  user = await user.save();
+
+  if (!user) return res.status(404).send("The user cannot be created");
+
+  res.send(user);
 });
 
 module.exports = router;
