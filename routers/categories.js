cc523caaf60477cffdb06f5a46b93585b66a50d3
Ariq Daffa <ariq.d.harahap@banksinarmas.com>
2021-02-02 09:25:57 +0700

    update category

--- routers/categories.js
+++ routers/categories.js
@@ -2,6 +2,7 @@ const { Category } = require("../models/category");
 const express = require("express");
 const router = express.Router();
 
+// Get all categories
 router.get(`/`, async (req, res) => {
   const categoryList = await Category.find();
 
@@ -12,6 +13,7 @@ router.get(`/`, async (req, res) => {
   res.status(200).send(categoryList);
 });
 
+// Get category by ID
 router.get("/:id", async (req, res) => {
   const category = await Category.findById(req.params.id);
 
@@ -23,6 +25,7 @@ router.get("/:id", async (req, res) => {
   res.status(200).send(category);
 });
 
+// Insert category
 router.post("/", async (req, res) => {
   let category = new Category({
     name: req.body.name,
@@ -37,6 +40,26 @@ router.post("/", async (req, res) => {
   res.send(category);
 });
 
+// Update category
+router.put("/:id", async (req, res) => {
+  const category = await Category.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      icon: req.body.icon,
+      color: req.body.color,
+    },
+    {
+      new: true, // return the updated data instead of old data
+    }
+  );
+
+  if (!category) return res.status(404).send("The category cannot be created");
+
+  res.send(category);
+});
+
+// Delete category
 // /api/v1/asdasd-id
 router.delete("/:id", (req, res) => {
   Category.findByIdAndRemove(req.params.id)
