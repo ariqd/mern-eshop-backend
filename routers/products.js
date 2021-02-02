edbf109d35c52ee25dea35e495837e75562e9077
Ariq Daffa <ariq.d.harahap@banksinarmas.com>
2021-02-02 10:15:02 +0700

    insert product

--- routers/products.js
+++ routers/products.js
@@ -1,7 +1,9 @@
 const { Product } = require("../models/product");
+const { Category } = require("../models/category");
 const express = require("express");
 const router = express.Router();
 
+// Get all products
 router.get(`/`, async (req, res) => {
   const productList = await Product.find();
 
@@ -12,26 +14,45 @@ router.get(`/`, async (req, res) => {
   res.send(productList);
 });
 
-router.post(`/`, (req, res) => {
+// Insert product
+router.post(`/`, async (req, res) => {
   // const newProduct = req.body;
   // console.log("newProduct", newProduct);
-  const product = new Product({
+  const category = await Category.findById(req.body.category);
+
+  if (!category) return res.status(400).send("Invalid category");
+
+  let product = new Product({
     name: req.body.name,
+    description: req.body.description,
+    richDescription: req.body.richDescription,
     image: req.body.image,
+    brand: req.body.brand,
+    price: req.body.price,
+    category: req.body.category,
     countInStock: req.body.countInStock,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews,
+    isFeatured: req.body.isFeatured,
   });
 
-  product
-    .save()
-    .then((createdProduct) => {
-      res.status(201).json(createdProduct);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-        success: false,
-      });
-    });
+  product = await product.save();
+
+  if (!product) return res.status(500).send("The products cannot be created");
+
+  return res.status(201).send(product);
+
+  // product
+  //   .save()
+  //   .then((createdProduct) => {
+  //     res.status(201).json(createdProduct);
+  //   })
+  //   .catch((err) => {
+  //     res.status(500).json({
+  //       error: err,
+  //       success: false,
+  //     });
+  //   });
 
   // res.send(newProduct);
 });
