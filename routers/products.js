4c537457b181d013c86f7ad5053b352afd0cfd7f
Ariq Daffa <ariq.d.harahap@banksinarmas.com>
2021-02-02 13:57:41 +0700

    update product

--- routers/products.js
+++ routers/products.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 // Get all products
 router.get(`/`, async (req, res) => {
-  const productList = await Product.find().populate('category');
+  const productList = await Product.find().populate("category");
   // .select("name image -_id");
 
   if (!productList) {
@@ -17,7 +17,9 @@ router.get(`/`, async (req, res) => {
 
 // Get a single product
 router.get(`/:id`, async (req, res) => {
-  const productList = await Product.findById(req.params.id).populate('category');
+  const productList = await Product.findById(req.params.id).populate(
+    "category"
+  );
 
   if (!productList) {
     res.status(500).json({ success: false });
@@ -67,6 +69,37 @@ router.post(`/`, async (req, res) => {
   //   });
 
   // res.send(newProduct);
+});
+
+// Update product
+router.put("/:id", async (req, res) => {
+  const category = await Category.findById(req.body.category);
+
+  if (!category) return res.status(400).send("Invalid category");
+
+  const product = await Product.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      description: req.body.description,
+      richDescription: req.body.richDescription,
+      image: req.body.image,
+      brand: req.body.brand,
+      price: req.body.price,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+      isFeatured: req.body.isFeatured,
+    },
+    {
+      new: true, // return the updated data instead of old data
+    }
+  );
+
+  if (!product) return res.status(404).send("The product cannot be created");
+
+  res.status(200).send(product);
 });
 
 module.exports = router;
