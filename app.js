e0ddf6d48b3788a0081466b8c1d8aaae4ed54c8b
Ariq Daffa <ariq.d.harahap@banksinarmas.com>
2021-02-03 10:02:28 +0700

    error handler, jwt routes

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
+require("dotenv/config");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const cors = require('cors');
+const cors = require("cors");
+const authJwt = require("./helpers/jwt");
+const errorHandler = require('./helpers/error-handler');
 
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 
-require("dotenv/config");
-
-// Middleware
+// Middlewares
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
+app.use(authJwt());
+app.use(errorHandler);
 
 // Routers
 const productsRouter = require("./routers/products");
@@ -42,4 +45,3 @@ app.listen(3000, () => {
   // console.log(api);
   console.log("server is running at http://localhost:3000");
 });
-
