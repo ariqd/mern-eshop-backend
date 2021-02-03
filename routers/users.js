ce1510575dc002fc3460bd4d0ea8141c22c49122
Ariq Daffa <ariq.d.harahap@banksinarmas.com>
2021-02-03 15:22:41 +0700

    update, delete, get count users

--- routers/users.js
+++ routers/users.js
@@ -47,6 +47,58 @@ router.post(`/`, async (req, res) => {
   res.send(user);
 });
 
+// Update user
+router.put("/:id", async (req, res) => {
+  const userExist = await User.findById(req.params.id);
+  let newPassword;
+  if (req.body.password) {
+    newPassword = bcrypt.hashSync(req.body.password, 10);
+  } else {
+    newPassword = userExist.passwordHash;
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+      passwordHash: newPassword,
+      phone: req.body.phone,
+      isAdmin: req.body.isAdmin,
+      street: req.body.street,
+      apartment: req.body.apartment,
+      zip: req.body.zip,
+      city: req.body.city,
+      country: req.body.country,
+    },
+    { new: true }
+  );
+
+  if (!user) return res.status(400).send("the user cannot be created!");
+
+  res.send(user);
+});
+
+// Delete user
+router.delete("/:id", (req, res) => {
+  User.findByIdAndRemove(req.params.id)
+    .then((user) => {
+      if (user)
+        return res.status(200).json({
+          success: true,
+          message: "The user is deleted!",
+        });
+
+      return res
+        .status(404)
+        .json({ success: false, message: "The user is not found" });
+    })
+    .catch((err) => {
+      return res.status(400).json({ success: false, error: err });
+    });
+});
+
+// User login
 router.post("/login", async (req, res) => {
   // Get user by email
   const user = await User.findOne({ email: req.body.email });
@@ -69,6 +121,39 @@ router.post("/login", async (req, res) => {
   return res.status(200).send({
     user: user.email,
     token: token,
+  });
+});
+
+// Register user
+router.post(`/register`, async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    street: req.body.street,
+    apartement: req.body.apartement,
+    city: req.body.city,
+    zip: req.body.zip,
+    country: req.body.country,
+    isAdmin: req.body.isAdmin,
+  });
+
+  user = await user.save();
+
+  if (!user) return res.status(404).send("The user cannot be created");
+
+  res.send(user);
+});
+
+// Get users count
+router.get("/get/count", async (req, res) => {
+  const userCount = await User.countDocuments((count) => count);
+
+  if (!userCount) return res.status(500).json({ success: false });
+
+  return res.send({
+    userCount: userCount,
   });
 });
 
